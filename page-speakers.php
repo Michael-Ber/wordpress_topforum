fd22718ca20aebd7554184ab78e61160e57203f6
Michael <example@gmail.com>
2021-05-17 21:21:01 +0800

    page speakers done, need to do single page for speaker detail

--- page-speakers.php
+++ page-speakers.php
@@ -12,37 +12,70 @@ Template Name: Speakers
 
     <section class="speakers">
         <div class="container">
-            <h1 class="speakers__title title">OUR speakers</h1>
-            <span class="speakers__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
-            <h2 class="speakers__subtitle">please select a conference</h2>
+            <h1 class="speakers__title title"><?php the_title(); ?></h1>
+            <span class="speakers__text"><?php the_field('speakers_text'); ?></span>
+            <h2 class="speakers__subtitle"><?php the_field('speakers_select'); ?></h2>
             <div class="speakers__choose">
                 <select name="conf" id="conf" class="speakers__select">
                     <option disabled selected value="" class="exhibitors__option">Wealth TOP FORUM Israel 2016</option>
-                    <option value="option1" class="speakers__option">option1</option>
-                    <option value="option2" class="speakers__option">option2</option>
-                    <option value="option3" class="speakers__option">option3</option>
-                    <option value="option4" class="speakers__option">option4</option>
-                    <option value="option5" class="speakers__option">option5</option>
+                    <?php 
+                        $posts = get_posts( array(
+                            'numberposts' => -1,
+                            'category_name'    => 'conferences',
+                            'orderby'     => 'date',
+                            'order'       => 'ASC',
+                            'post_type'   => 'post',
+                            'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+                        ) );
+
+                        foreach( $posts as $post ){
+                            setup_postdata($post);
+                    ?>
+                        <option value="<?php the_title(); ?>" class="sponsors__option"><?php the_title(); ?></option>
+                    <?php
+                        }
+
+                        wp_reset_postdata(); // сброс
+                    ?>
                 </select>
             </div>
             <div class="speakers__items">
-                <div class="speakers__item">
-                    <div class="speakers__item-logo">
-                        <div class="speakers__item-logo-wrapper">
-                            <img src="./assets/img/speakers/speaker1.jpg" alt="logo1">
+                <?php 
+                    $posts = get_posts( array(
+                        'numberposts' => -1,
+                        'category_name'    => 'speakers_item',
+                        'orderby'     => 'date',
+                        'order'       => 'ASC',
+                        'post_type'   => 'post',
+                        'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+                    ) );
+
+                    foreach( $posts as $post ){
+                        setup_postdata($post);
+                ?>
+                    <div class="speakers__item">
+                        <div class="speakers__item-logo">
+                            <div class="speakers__item-logo-wrapper">
+                                <img src="<?php the_field('speakers_card-img'); ?>" alt="logo1">
+                            </div>
+                        </div>
+                        <div class="speakers__item-title"><?php the_title(); ?></div>
+                        <div class="speakers__item-text">
+                            <span><?php the_field('speakers_card-text'); ?></span>
+                        </div>
+                        <div class="speakers__item-btn btn">
+                            <a href="<?php echo get_permalink(); ?>">
+                                Learn more
+                            </a>
                         </div>
                     </div>
-                    <div class="speakers__item-title">Liam Noah</div>
-                    <div class="speakers__item-text">
-                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</span>
-                    </div>
-                    <div class="speakers__item-btn btn">
-                        <a href="./speakerdetail.html">
-                            Learn more
-                        </a>
-                    </div>
-                </div>
-                <div class="speakers__item">
+                <?php
+                    }
+
+                    wp_reset_postdata(); // сброс
+                ?>
+                
+                <!-- <div class="speakers__item">
                     <div class="speakers__item-logo">
                         <div class="speakers__item-logo-wrapper">
                             <img src="./assets/img/speakers/speaker2.jpg" alt="logo2">
@@ -137,7 +170,7 @@ Template Name: Speakers
                             Learn more
                         </a>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
